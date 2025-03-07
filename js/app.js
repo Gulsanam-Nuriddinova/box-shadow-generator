b8cf92c2d300739ae923bdac4af781f45d8882c4
Gulsanam-Nuriddinova <gulsanamnuriddinova56@gmail.com>
2025-03-07 23:10:23 +0500

    add responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 const elBox = document.getElementById('box');
 const elColor = document.getElementById('input-color');
 const elInputHorizontal = document.getElementById('horizontal-shadow');
@@ -18,9 +17,41 @@ function updateBoxShadow() {
 
     elBox.style.boxShadow = `${horizontal} ${vertical} ${blur} ${spread} rgba(${parseInt(color.slice(1, 3), 16)}, ${parseInt(color.slice(3, 5), 16)}, ${parseInt(color.slice(5, 7), 16)}, ${opacity})`;
 
-    
-    elBox2.innerHTML = `box-shadow: ${horizontal} ${vertical} ${blur} ${spread} rgba(${parseInt(color.slice(1, 3), 16)}, ${parseInt(color.slice(3, 5), 16)}, ${parseInt(color.slice(5, 7), 16)}, ${opacity});`;
+    elBox2.innerHTML = `
+   
+        <p>box-shadow: ${horizontal} ${vertical} ${blur} ${spread} rgba(${parseInt(color.slice(1, 3), 16)}, ${parseInt(color.slice(3, 5), 16)}, ${parseInt(color.slice(5, 7), 16)}, ${opacity});</p>
+        <button onclick="copyText()" style="background-color: #6f1728; margin-top:10px; margin-left:224px;  color: white; border: none; padding: 10px 20px; font-size: 16px; cursor: pointer; border-radius: 5px; transition: background-color 0.3s ease;">Copy</button>
+    `;
 }
+
+function copyText() {
+    const textToCopy = elBox2.querySelector("p").innerText;
+    const tempInput = document.createElement("textarea");
+    tempInput.value = textToCopy;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    const message = document.createElement("div");
+    message.textContent = "Code copied! ✅";
+    message.style.position = "fixed";
+    message.style.bottom = "25px";
+    message.style.left = "900px";
+    message.style.background = "#6f1728";
+    message.style.color = "white";
+    message.style.padding = "10px 20px";
+    message.style.borderRadius = "5px";
+    message.style.boxShadow = "0 0 10px rgba(0,0,0,0.3)";
+    message.style.zIndex = "1000";
+
+    document.body.appendChild(message);
+
+    setTimeout(() => {
+        message.remove();
+    }, 3000);
+}
+
+
 
 
 elColor.addEventListener('input', updateBoxShadow);
@@ -29,6 +60,5 @@ elVertical.addEventListener('input', updateBoxShadow);
 elBlur.addEventListener('input', updateBoxShadow);
 elSpread.addEventListener('input', updateBoxShadow);
 elOppacity.addEventListener('input', updateBoxShadow);
-
 
 updateBoxShadow();
